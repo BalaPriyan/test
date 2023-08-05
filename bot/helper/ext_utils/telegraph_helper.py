d849d5c55e56db479adbfaa157000a1b4e76f553
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-08-05 04:12:27 -0700

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -4,11 +4,11 @@ from asyncio import sleep
 from telegraph.aio import Telegraph
 from telegraph.exceptions import RetryAfterError
 
-from bot import LOGGER, bot_loop
+from bot import LOGGER, bot_loop, config_dict
+
 
 class TelegraphHelper:
     def __init__(self, author_name=None, author_url=None):
-        self.__error = False
         self.telegraph = Telegraph(domain='graph.org')
         self.short_name = ''.join(SystemRandom().choices(ascii_letters, k=8))
         self.access_token = None
@@ -16,23 +16,15 @@ class TelegraphHelper:
         self.author_url = author_url
 
     async def create_account(self):
+        await self.telegraph.create_account(
+            short_name=self.short_name,
+            author_name=self.author_name,
+            author_url=self.author_url
+        )
+        self.access_token = self.telegraph.get_access_token()
         LOGGER.info("Creating Telegraph Account")
-        try:
-            await self.telegraph.create_account(
-                short_name=self.short_name,
-                author_name=self.author_name,
-                author_url=self.author_url
-            )
-            self.access_token = self.telegraph.get_access_token()
-            self.__error = False
-        except Exception as e:
-            self.__error = True
-            LOGGER.error(e)
 
     async def create_page(self, title, content):
-        if self.__error:
-            LOGGER.info('Telegraph is not working')
-            return
         try:
             return await self.telegraph.create_page(
                 title=title,
@@ -47,9 +39,6 @@ class TelegraphHelper:
             return await self.create_page(title, content)
 
     async def edit_page(self, path, title, content):
-        if self.__error:
-            LOGGER.info('Telegraph is not working')
-            return
         try:
             return await self.telegraph.edit_page(
                 path=path,
@@ -65,42 +54,29 @@ class TelegraphHelper:
             return await self.edit_page(path, title, content)
 
     async def edit_telegraph(self, path, telegraph_content):
-        if self.__error:
-            LOGGER.info('Telegraph is not working')
-            return
         nxt_page = 1
         prev_page = 0
         num_of_path = len(path)
         for content in telegraph_content:
             if nxt_page == 1:
-                content += f'<b><a href="https://graph.org/{path[nxt_page]}">Next</a></b>'
+                content += f'<b><a href="https://telegra.ph/{path[nxt_page]}">Next</a></b>'
                 nxt_page += 1
             else:
                 if prev_page <= num_of_path:
-                    content += f'<b><a href="https://graph.org/{path[prev_page]}">Prev</a></b>'
+                    content += f'<b><a href="https://telegra.ph/{path[prev_page]}">Prev</a></b>'
                     prev_page += 1
                 if nxt_page < num_of_path:
-                    content += f'<b> | <a href="https://graph.org/{path[nxt_page]}">Next</a></b>'
+                    content += f'<b> | <a href="https://telegra.ph/{path[nxt_page]}">Next</a></b>'
                     nxt_page += 1
             await self.edit_page(
                 path=path[prev_page],
-                title='Z Torrent Search',
+                title=f"{config_dict['TITLE_NAME']} Torrent Search",
                 content=content
             )
         return
 
-    async def revoke_access_token(self):
-        if self.__error:
-            LOGGER.info('Telegraph is not working')
-            return
-        LOGGER.info('Revoking telegraph access token...')
-        try:
-            return await self.telegraph.revoke_access_token()
-        except Exception as e:
-            LOGGER.error(
-                f'Failed Revoking telegraph access token due to : {e}')
 
+telegraph = TelegraphHelper(config_dict['AUTHOR_NAME'],
+                            config_dict['AUTHOR_URL'])
 
-telegraph = TelegraphHelper(
-    'Z-Mirror', 'https://github.com/Dawn-India/Z-Mirror')
 bot_loop.run_until_complete(telegraph.create_account())
