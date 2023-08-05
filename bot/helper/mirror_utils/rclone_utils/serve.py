a1c863d6b14b1dbec7d8f030f89f8e9bc1b7515f
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-08-05 04:08:13 -0700

    Update serve.py

--- bot/helper/mirror_utils/rclone_utils/serve.py
+++ bot/helper/mirror_utils/rclone_utils/serve.py
@@ -9,7 +9,7 @@ RcloneServe = []
 
 
 async def rclone_serve_booter():
-    if not config_dict['RCLONE_SERVE_URL'] or not await aiopath.exists('rcl.conf'):
+    if not config_dict['RCLONE_SERVE_URL'] or not await aiopath.exists('rclone.conf'):
         if RcloneServe:
             try:
                 RcloneServe[0].kill()
@@ -18,7 +18,7 @@ async def rclone_serve_booter():
                 pass
         return
     config = ConfigParser()
-    async with aiopen('rcl.conf', 'r') as f:
+    async with aiopen('rclone.conf', 'r') as f:
         contents = await f.read()
         config.read_string(contents)
     if not config.has_section('combine'):
@@ -27,7 +27,7 @@ async def rclone_serve_booter():
         config.add_section('combine')
         config.set('combine', 'type', 'combine')
         config.set('combine', 'upstreams', upstreams)
-        with open('rcl.conf', 'w') as f:
+        with open('rclone.conf', 'w') as f:
             config.write(f, space_around_delimiters=False)
     if RcloneServe:
         try:
@@ -35,10 +35,10 @@ async def rclone_serve_booter():
             RcloneServe.clear()
         except:
             pass
-    cmd = ["zcl", "serve", "http", "--config", "rcl.conf", "--no-modtime",
-            "combine:", "--addr", f":{config_dict['RCLONE_SERVE_PORT']}",
-            "--vfs-cache-mode", "full", "--vfs-cache-max-age", "1m0s",
-            "--buffer-size", "64M"]
+    cmd = ["rclone", "serve", "http", "--config", "rclone.conf", "--no-modtime",
+           "combine:", "--addr", f":{config_dict['RCLONE_SERVE_PORT']}",
+           "--vfs-cache-mode", "full", "--vfs-cache-max-age", "1m0s",
+           "--buffer-size", "64M"]
     if (user := config_dict['RCLONE_SERVE_USER']) and (pswd := config_dict['RCLONE_SERVE_PASS']):
         cmd.extend(("--user", user, "--pass", pswd))
     rcs = await create_subprocess_exec(*cmd)
