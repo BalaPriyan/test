9f6f116c66770f82314ecb2deba18cf63460d4f1
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-08-05 04:34:54 -0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -724,7 +724,7 @@ if not ospath.exists('.netrc'):
 zrun(["chmod", "600", ".netrc"])
 zrun(["cp", ".netrc", "/root/.netrc"])
 
-trackers = check_output("curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt https://ngosang.github.io/trackerslist/trackers_all_http.txt https://newtrackon.com/api/all https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_tracker.txt | awk '$0' | tr '\n\n' ','", shell=True).decode('utf-8').rstrip(',')
+trackers = check_output("curl -Ns https://ngosang.github.io/trackerslist/trackers_all_http.txt | awk '$0' | tr '\n\n' ',' ", shell=True).decode('utf-8').rstrip(',')
 with open("a2c.conf", "a+") as a:
     if TORRENT_TIMEOUT is not None:
         a.write(f"bt-stop-timeout={TORRENT_TIMEOUT}\n")
